14aa72dba01341c88db63da1a43c6eed826313c4
Sweatpotatos <119827177+Sweatpotatos@users.noreply.github.com>
2024-09-16 21:53:18 -0700

    Update corrected_minimum_template.s

--- 3-sort/corrected_minimum_template.s
+++ 3-sort/corrected_minimum_template.s
@@ -1,7 +1,7 @@
 # Exercise 3
 # Max Score: 12 points
 #
-# Students: 
+# Students: Yaolei Bian, Allen Leinberger, Muhammud
 #
 # minimum.s 
 # Finds the index of the smallest element in an integer array
