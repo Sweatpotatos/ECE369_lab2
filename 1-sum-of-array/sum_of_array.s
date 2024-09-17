c8f122d7a4fd9ccfe132081fde8df674a9f0743e
Sweatpotatos <119827177+Sweatpotatos@users.noreply.github.com>
2024-09-16 21:53:03 -0700

    Update sum_of_array.s

--- 1-sum-of-array/sum_of_array.s
+++ 1-sum-of-array/sum_of_array.s
@@ -2,7 +2,7 @@
 # 'sum_of_array.s' - This program performs the sum of the array
 #  Max Score: 5 points
 #
-# Students: 
+# Students: Students: Yaolei Bian, Allen Leinberger, Muhammud
 #
 #
 # Insert your answers below each question:-
